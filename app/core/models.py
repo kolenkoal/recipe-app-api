555a980aade0b3c38b71871e578ddf5c9a69e6c6
Alexander <kolenko.alexander0@gmail.com>
2023-08-31 10:56:34 +0300

    Added api/recipe/ endpoint

--- app/core/models.py
+++ app/core/models.py
@@ -52,7 +52,7 @@ class Recipe(models.Model):
         on_delete=models.CASCADE,
     )
 
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
