ad31f46508fe102b1aaa6637ee614fffdec2cd8a
Alexander <kolenko.alexander0@gmail.com>
2023-08-31 13:55:57 +0300

    Added a feature to create new tags when creating a recipe

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -94,4 +94,3 @@ class PrivateTagsAPITests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
         self.assertFalse(Tag.objects.filter(id=tag.id).exists())
-        # self.assertFalse(Tag.objects.filter(user=self.user).exists())
